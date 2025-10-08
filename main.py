9f0c1a1ec90ae4a52ef504f86b5244ee114efe23
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-10-08 08:35:15 -0300

    feat(project): create player class and draw a player in the game

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pygame
-from constants import *
+
+from constants import REFRESH_RATE, SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -7,21 +11,26 @@ def main():
     dt = 0
     running = True
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while running:
         screen.fill("black")
+
+        player.draw(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         pygame.display.flip()
-        
+
         last_call = clock.tick(REFRESH_RATE)
         dt = last_call / 1000
     # print("Starting Asteroids!")
     # print(f"Screen width: {SCREEN_WIDTH}")
     # print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
