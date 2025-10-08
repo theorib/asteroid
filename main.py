b5d3be7f8453034040cdcfff0bde05f04eb6c305
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-10-08 08:12:40 -0300

    ✨ feat(project): set up initial project structure and game window
    - initialize pygame with a basic game window and event loop
    - define core game constants for screen dimensions and asteroid properties
    - set up project metadata and dependencies using pyproject.toml
    - generate uv.lock for reproducible dependency management
    - add .gitignore to exclude common Python artifacts and virtual environments
    - specify Python version 3.13 for the project
    - create an empty README.md for future project documentation

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+from constants import *
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+
+    while running:
+        screen.fill("black")
+        pygame.display.flip()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+    # print("Starting Asteroids!")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
