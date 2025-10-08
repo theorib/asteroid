8a7bfc1407345d84aa5e75408d39250ead1b10ba
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-10-08 08:20:49 -0300

    feat(project): limit game refresh rate to 60fps

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     running = True
 
     while running:
         screen.fill("black")
-        pygame.display.flip()
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        pygame.display.flip()
+        
+        last_call = clock.tick(REFRESH_RATE)
+        dt = last_call / 1000
     # print("Starting Asteroids!")
     # print(f"Screen width: {SCREEN_WIDTH}")
     # print(f"Screen height: {SCREEN_HEIGHT}")
